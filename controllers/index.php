b9eb4a0e31143eeef6c615c39be4b6117986fc4d
Kacper Koziol <kacperu770@gmail.com>
2019-11-05 23:38:00 +0100

    A little change

--- controllers/index.php
+++ controllers/index.php
@@ -1,20 +1,44 @@
 <?php
 
+
+session_start();
 require('../vendor/autoload.php');
+include('../config.php');
+
+
+
+$config = new DBConfig\DBConfig();
+
+$dbConf = $config->getConfig();
+$dbConfig = $dbConf->fetch_assoc();
+
+// $db = new mysqli('localhost', 'root', 'vertrigo', 'data_on_tables');
+$db = new mysqli($dbConfig);
+$dbQuery = $db->query("SELECT * FROM clients");
+$dbResult = $dbQuery->fetch_assoc();
+
+foreach ($dbResult as $value) {
+    printf($value);
+} 
+
+
+
+
+
+
+
+
+
+
+
 
 $loader = new Twig_Loader_Filesystem('../views');
 $twig = new Twig_Environment($loader);
 
-$title = 'Cycki Marioli';
-$title2 = 'Muffinki~~';
-
 echo $twig->render('index.html', array(
     'alert' => 'Błąd walidacji danych',
     'age' => 3,
-    'title' => $title,
-    'title2' => $title2,
     
 ));
-
 
 ?>
