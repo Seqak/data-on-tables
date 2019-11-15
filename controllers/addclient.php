d0a884162d02cdf12b8743e5aa5535e2c0a68eb5
Kacper Koziol <kacperu770@gmail.com>
2019-11-15 01:47:13 +0100

    Add to new view datapicker, select2, some script and name elements

--- controllers/addclient.php
+++ controllers/addclient.php
@@ -4,13 +4,21 @@ session_start();
 require('../vendor/autoload.php');
 require('../model/dbconnect.php');
 
-
+if (isset($_POST['clientAddButton'])) {
+    if (isset($_POST['clientName'])){
+        // echo '<span style="margin-left: 300px;">' . $_POST['clientName']. '</span>';
+    }
+}
 
 
 
 $loader = new Twig_Loader_Filesystem('../views');
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('addclient.html', array());
+echo $twig->render('addclient.html', array(
+
+    'fieldsEmpty' => "some",
+
+));
 
 ?>
